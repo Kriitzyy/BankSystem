809c54cf3e4a58b7d0235508aebbcc21455bb28a
kriitzyy <Akadmn04@gmail.com>
2026-03-06 19:02:20 +0100

    A ToDo branch that includes what to do and short project explaniation

--- obj/Debug/net8.0/BankSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/BankSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed626d89c2f62df2dcef55a2e247d22168628ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
